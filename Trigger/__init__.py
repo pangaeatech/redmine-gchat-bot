2760aa7e1a62deb5a744420919804556b824c5b3
Michael Waddell <michael@waddellnet.com>
2022-03-19 18:21:58 -0500

    verbose logging

--- Trigger/__init__.py
+++ Trigger/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed: %s' % jsons.dumps(req))
 
     try:
         event = req.get_json()
