d5508c00ece5a56bf553fe344973888a8eb9ef50
Michael Waddell <michael@waddellnet.com>
2022-03-19 18:29:42 -0500

    Revert "verbose logging"
    This reverts commit 2760aa7e1a62deb5a744420919804556b824c5b3.

--- Trigger/__init__.py
+++ Trigger/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed: %s' % jsons.dumps(req))
+    logging.info('Python HTTP trigger function processed a request.')
 
     try:
         event = req.get_json()
